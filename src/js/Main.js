a4f970ed89672290085bb574d707268377544717
ubiv64 <ubiv64@gmail.com>
2020-02-24 18:36:26 -0600

    clean up, menu

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,25 +1,16 @@
-import Slider from './Slider';
+import Menu from './Menu';
 
 import React, { Component } from 'react';
 import '../styles/Main.css';
 
 class Main extends Component {
   render() {
-
-    var array = ["steph","kd","klay"];
-
     return (
       <div>
       <div className="header">
-        <header>
-        <h1> proto </h1>
-        </header>
       </div>
-        <div className="body">
-          <p> proto dot com </p>
-        </div>
-        <div className="slider">
-          <Slider array={array} />
+        <div className="menu">
+          <Menu />
         </div>
       </div>
     );
---
+++ src/js/Menu.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function Menu() {
+  return (
+    <Router>
+      <div className="menu">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">recents</Link>
+            </li>
+            <li>
+              <Link to="/work">work</Link>
+            </li>
+            <li>
+              <Link to="/feed">feed</Link>
+            </li>
+            <li>
+              <Link to="/contact">contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/work">
+            <Work />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/feed">
+            <Feed />
+          </Route>
+          <Route path="/">
+            <Recents />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Work() {
+  return (
+  	<div>
+  	<h2>work</h2>
+  	<h3>vibu anbarasan</h3>
+  	</div>
+  );
+}
+
+function Contact() {
+  return <h2>contact</h2>
+}
+
+function Feed() {
+  return <h2>feed</h2>;
+}
+
+function Recents() {
+  return <h2>recents</h2>;
+}
