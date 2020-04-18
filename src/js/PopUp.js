d13ab9af84a802fb8f7edf872b0ae13eeda45617
ubiv64 <ubiv64@gmail.com>
2020-04-18 12:22:09 -0500

    closer to it, but not quite there yet

--- src/js/PopUp.js
+++ src/js/PopUp.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
+import '../styles/PopUp.css';
+
 class PopUp extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {index: 0}
+	}
+
+	handleClick(){
+		this.setState(state => {
+			return (state.index === this.props.images.length-1) ? {index: 0} : {index: state.index+1}
+		});
+	}
+
+	renderSlides(images){
+		return(
+			<button className='button' onClick={() => this.handleClick()}>
+				{images[this.state.index]}
+			</button>
+		);
 	}
 
 	render() {
+		const popUp = this.renderSlides(this.props.images)
 
-		return()
+		return(
+			<div>{popUp}</div>
+		);
 	}
 }
+
+export default PopUp
--- src/js/RecentsGrid.js
+++ src/js/RecentsGrid.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 import '../styles/RecentsGrid.css';
 
+import PopUp from './PopUp.js';
+
 class RecentsGrid extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {isClicked: false}
 	}
 
 	//pass in handleClick()
@@ -13,22 +16,41 @@ class RecentsGrid extends Component {
 		//may not need it since random is passed in as props
 	createGrid(random) {
 		let images = random.map(int => {
-			 return <img src={`https://vibu-a.s3.us-east-2.amazonaws.com/${int}.jpg`} />;
+			 return <img src={`https://vibu-a.s3.us-east-2.amazonaws.com/${int}.jpg`} onClick={this.handlePopUpClick} />;
 		});
 
-		return images
+		return images;
 	}
 
-	// handClick = () => {
-		//pop-up modal with img.src array as props
-	// }
+	invokePopUp(grid){
+		document.getElementById("vibu").style.opacity=.1
+
+		return <PopUp images={grid} />
+	}
+
+	handlePopUpClick = () => {
+		this.setState({isClicked: true})
+	}
+
+	handleClick = () => {
+		this.setState({isClicked: false})
+	}
 
 	render () {
-		const imageGrid = this.createGrid(this.props.random)
+		const grid = this.createGrid(this.props.random)
+
+		let popUp = (this.state.isClicked) ? this.invokePopUp(grid) : null
+
+		console.log(this.state.isClicked)
 
 		return (
-			<div className="container">
-				{imageGrid} 
+			<div>
+				<div className="outside">
+					{popUp}
+				</div>
+				<div id="vibu" className="container">
+					{grid}
+				</div>
 			</div>
 		);
 	}
