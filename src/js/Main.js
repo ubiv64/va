9ff785c3e3d3fff20292a6f037c23ca215dc03fc
ubiv64 <ubiv64@gmail.com>
2020-11-02 18:52:30 -0600

    updating copy

--- src/js/Main.js
+++ src/js/Main.js
@@ -9,7 +9,7 @@ function Main() {
   return(
     <div>
       <p className="name"> Vibu Anbarasan </p>
-      <p className="about"> full stack dev with experience in writing service-oriented APIs and serverless frontend web applications. </p>
+      <p className="about"> full stack dev versed in design, writing single-page apps and service-oriented APIs. </p>
       <Theme />
     </div>
   )
